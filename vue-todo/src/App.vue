92951fe599a7c053928942a48aee6958109781c8
moonseo.kim <moonseo.kim@nhn.com>
2022-02-15 20:23:24 +0900

    완료, 정렬 기능 구현

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -11,6 +11,7 @@
           :index="index"
           :key="index"
           :todoItem="todoItem"
+          @toggle="toggleTodoItemComplete"
           @delete="deleteTodoItem"
         />
       </ul>
@@ -25,22 +26,27 @@ import TodoListItem from "./components/TodoListItem.vue";
 
 const STORAGE_KEY = "vue-todo-ts-v1";
 const storage = {
-  save(todoItems: any[]) {
+  save(todoItems: Todo[]) {
     const parsed = JSON.stringify(todoItems);
     localStorage.setItem(STORAGE_KEY, parsed);
   },
-  fetch() {
+  fetch(): Todo[] {
     const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
     return JSON.parse(todoItems);
   },
 };
+
+export interface Todo {
+  title: string;
+  done: boolean;
+}
 
 export default Vue.extend({
   components: { TodoInput, TodoListItem },
   data() {
     return {
       todoText: "",
-      todoItems: [] as any[],
+      todoItems: [] as Todo[],
     };
   },
   methods: {
@@ -48,7 +54,10 @@ export default Vue.extend({
       this.todoText = value;
     },
     addTodoItem() {
-      this.todoItems.push(this.todoText);
+      this.todoItems.push({
+        title: this.todoText,
+        done: false,
+      });
       storage.save(this.todoItems);
       this.initTodoText();
     },
@@ -56,7 +65,18 @@ export default Vue.extend({
       this.todoText = "";
     },
     fetchTodoItems() {
-      this.todoItems = storage.fetch();
+      this.todoItems = storage.fetch().sort((a, b) => {
+        if (a.title < b.title) return -1;
+        else if (a.title > b.title) return 1;
+        else return 0;
+      });
+    },
+    toggleTodoItemComplete(todoItem: Todo, index: number) {
+      this.todoItems.splice(index, 1, {
+        ...todoItem,
+        done: !todoItem.done,
+      });
+      storage.save(this.todoItems);
     },
     deleteTodoItem(index: number) {
       this.todoItems.splice(index, 1);
--- vue-todo/src/components/TodoListItem.vue
+++ vue-todo/src/components/TodoListItem.vue
@@ -1,23 +1,36 @@
 <template>
   <li>
-    <span>{{ todoItem }}</span>
+    <span class="item" :class="todoItemClass" @click="toggleItem">{{
+      todoItem.title
+    }}</span>
     <button @click="deleteTodo">삭제</button>
   </li>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue, { PropType } from "vue";
+import { Todo } from "../App.vue";
 
 export default Vue.extend({
   props: {
     todoItem: {
-      type: String,
+      type: Object as PropType<Todo>,
     },
     index: {
       type: Number,
     },
   },
+
+  computed: {
+    todoItemClass(): string | null {
+      return this.todoItem.done ? "complete" : null;
+    },
+  },
+
   methods: {
+    toggleItem() {
+      this.$emit("toggle", this.todoItem, this.index);
+    },
     deleteTodo() {
       this.$emit("delete", this.index);
     },
@@ -25,4 +38,11 @@ export default Vue.extend({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.item {
+  cursor: pointer;
+}
+.complete {
+  text-decoration: line-through;
+}
+</style>
