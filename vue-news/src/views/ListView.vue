a978c23a0e53e2a70dba1ee6d73428746ea08984
moonseo.kim <moonseo.kim@nhn.com>
2022-02-22 20:30:14 +0900

    axios ts 전환

--- vue-news/src/views/ListView.vue
+++ vue-news/src/views/ListView.vue
@@ -1,19 +1,36 @@
 <template>
   <div>
-    <list-item></list-item>
+    <ListItem :items="newsItems"></ListItem>
   </div>
 </template>
 
-<script>
-import ListItem from '../components/ListItem.vue';
+<script lang="ts">
+import Vue from "vue";
+import { fetchNews, NewsItem } from "@/api";
+import ListItem from "../components/ListItem.vue";
 
-export default {
+export default Vue.extend({
   components: {
-    ListItem
+    ListItem,
   },
-}
+
+  data() {
+    return {
+      newsItems: [] as NewsItem[],
+    };
+  },
+
+  methods: {
+    async fetchNewsItems() {
+      const response = await fetchNews();
+      this.newsItems = response.data;
+    },
+  },
+
+  created() {
+    this.fetchNewsItems();
+  },
+});
 </script>
 
-<style>
-
-</style>
+<style></style>
