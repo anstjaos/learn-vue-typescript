fbc6aa5dfa29429faaf573195d46a221fe6ccf4b
moonseo.kim <moonseo.kim@nhn.com>
2022-02-23 18:18:40 +0900

    vuex state, mutation 전환

---
+++ vue-news/src/store/index.ts
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import Vuex, { StoreOptions } from "vuex";
+import { state, RootState } from "./state";
+import getters from "./getters.js";
+import { mutations } from "./mutations";
+import actions from "./actions.js";
+
+Vue.use(Vuex);
+
+const store: StoreOptions<RootState> = {
+  state: state,
+  mutations: mutations,
+};
+
+export default new Vuex.Store(store);
+
+// export default new Vuex.Store({
+//   strict: process.env.NODE_ENV !== 'production',
+//   state: {
+//     news: [],
+//     ask: [],
+//     jobs: [],
+//     user: {},
+//     item: {},
+//     list: [],
+//   },
+//   getters,
+//   mutations,
+//   actions,
+// })
